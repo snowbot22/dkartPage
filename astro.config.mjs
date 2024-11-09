1079c206ffc65c4a78664cfc4898c065d17cbcf9
Kevin Copo <kevincopo7@gmail.com>
2024-11-08 22:06:30 -0500

    GHA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site:"https://snowbot22.github.io",
+    base: "dkartPage"
+});
