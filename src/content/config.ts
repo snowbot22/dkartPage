cb37418a4b6fe5479da81bf7a61d0e8f9d656c58
Kevin Copo <kevincopo7@gmail.com>
2024-11-08 22:01:20 -0500

    t1

---
+++ src/content/config.ts
@@ -0,0 +1,19 @@
+// 1. Importa las utilidades de `astro:content`
+import { z,defineCollection } from 'astro:content';
+import { string } from 'astro:schema';
+// 2. Define tu colección(es)
+const pageCollection = defineCollection({ 
+  type: "content",
+  schema: z.object({ 
+    titulo: z.string(),
+    image: z.string(),
+    tag: z.string(),
+    desc: z.string()
+  }),
+
+ });
+// 3. Exporta un único objeto `collections` para registrar tu(s) colección(es)
+//    Esta clave debe coincidir con el nombre de tu directorio de colección en "src/content"
+export const collections = {
+  products: pageCollection,
+};
